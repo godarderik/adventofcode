5f6adb652e3720a0b6a5a650903ce1829fbac681
Erik Godard <godarderik@gmail.com>
2015-12-16 00:51:38 -0500

    slight modifications to problem 16

--- problem16.py
+++ problem16.py
@@ -1,13 +1,3 @@
-f = open("input16.txt")
-
-g  = open("inpt.txt")
-
-props = {}
-for line in g:
-    line = line.strip("\n")
-    line = line.split(" ")
-    props[line[0]] = int(line[1])
-
 def check(types, value):
     value = int(value)
     if types == "cats:" or types == "trees:":
@@ -16,8 +6,15 @@ def check(types, value):
         return props[types] > value
     return props[types] == value
 
-for line in f:
-    line = line.strip("\n")
-    line = line.split(" ")
-    if (check(line[2], line[3].strip(",")) and check(line[4], line[5].strip(",")) and check(line[6], line[7])):
-        print line 
+props = {}
+with open("inpt.txt") as g:
+    for line in g:
+        line = line.strip("\n").split(" ")
+        props[line[0]] = int(line[1])
+
+
+with open("input16.txt") as f:
+    for line in f:
+        line = line.strip("\n").split(" ")
+        if (check(line[2], line[3].strip(",")) and check(line[4], line[5].strip(",")) and check(line[6], line[7])):
+            print line 
