4d8ba3b5a70dd926c3a21b5a66f868101a40edd8
Erik Godard <godarderik@gmail.com>
2015-12-24 01:34:51 -0500

    further refinements

--- problem24.py
+++ problem24.py
@@ -6,16 +6,16 @@ nums = []
 with open('input24.txt') as f:
     for line in f:
         nums.append(int(line.strip("\n")))
-tot = sum(nums)/4
+tot = sum(nums)/5
 
 def hasSum(lst, sub):
-    quant = 1e9
+    quant = 1e99
     for y in range(1,len(lst)):
         for x in itertools.combinations(lst, y):
-            if sum(x) == tot and sub:
+            if sum(x) == tot and sub == 2:
                 return True
-            elif not sub and sum(x) == tot and hasSum(list(set(nums) - set(x)), True):
+            elif sub != 2 and sum(x) == tot and hasSum(list(set(lst) - set(x)), sub - 1):
                 quant = min(quant, reduce(operator.mul, x, 1))
-        if quant < 1e9:
+        if quant < 1e99:
             return quant
-print hasSum(nums, False)
+print hasSum(nums, 5)
