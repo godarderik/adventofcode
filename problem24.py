c6985b386d5ce4d0bfd4875ec5515ad47ca79116
Erik Godard <godarderik@gmail.com>
2015-12-24 01:13:24 -0500

    small change

--- problem24.py
+++ problem24.py
@@ -6,17 +6,16 @@ nums = []
 with open('input24.txt') as f:
     for line in f:
         nums.append(int(line.strip("\n")))
-tot = sum(nums)/3
+tot = sum(nums)/4
 
 def hasSum(lst, sub):
-    quant = 9999999999999
+    quant = 1e9
     for y in range(1,len(lst)):
         for x in itertools.combinations(lst, y):
             if sum(x) == tot and sub:
                 return True
             elif not sub and sum(x) == tot and hasSum(list(set(nums) - set(x)), True):
                 quant = min(quant, reduce(operator.mul, x, 1))
-        if quant < 9999999999999:
-            print quant 
-            break
-hasSum(nums, False)
+        if quant < 1e9:
+            return quant
+print hasSum(nums, False)
