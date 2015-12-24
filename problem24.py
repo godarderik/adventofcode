caa892b07c64f54f0632a0d7c768d510d44ff2fe
Erik Godard <godarderik@gmail.com>
2015-12-24 02:16:21 -0500

    performance improvement

--- problem24.py
+++ problem24.py
@@ -6,16 +6,17 @@ nums = []
 with open('input24.txt') as f:
     for line in f:
         nums.append(int(line.strip("\n")))
-tot = sum(nums)/5
+
+parts = 3
+tot = sum(nums)/parts
 
 def hasSum(lst, sub):
-    quant = 1e99
     for y in range(1,len(lst)):
         for x in itertools.combinations(lst, y):
             if sum(x) == tot and sub == 2:
                 return True
-            elif sub != 2 and sum(x) == tot and hasSum(list(set(lst) - set(x)), sub - 1):
-                quant = min(quant, reduce(operator.mul, x, 1))
-        if quant < 1e99:
-            return quant
-print hasSum(nums, 5)
+            elif sum(x) == tot and sub < parts:
+                return hasSum(list(set(lst) - set(x)), sub - 1)
+            elif sub == parts and sum(x) == tot and hasSum(list(set(lst) - set(x)), sub - 1) :
+                return reduce(operator.mul, x, 1)
+print hasSum(nums, parts)
