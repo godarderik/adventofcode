844076c19f2e7e572fa5722afc669385af4b4936
Erik Godard <godarderik@gmail.com>
2016-12-01 01:25:18 -0500

    clean up solution

--- 2016/problem1.py
+++ 2016/problem1.py
@@ -1,13 +1,11 @@
 import random
 import collections
 
-
 pos = (0,0)
 
 direction = 0
 
-
-locs = {}
+locs = set()
 first = []
 
 def nextDir(direc):
@@ -20,46 +18,27 @@ def nextDir(direc):
         direction -= 1
         if direction == -1:
             direction = 3
-def move(position, delta, direction):
-    if direction == 0:
-        for x in range(delta):
-            num = (position[0], position[1] + x)
-            if not num in locs:
-                locs[num] = 1
-            else:
-                first.append(num)
-        return (position[0], position[1] + delta)
-    elif direction == 1:
-        for x in range(delta):
-            num = (position[0] + x, position[1])
-            if not num in locs:
-                locs[num] = 1
-            else:
-                first.append(num)
-        return (position[0] + delta, position[1])
-    elif direction == 2:
-        for x in range(delta):
-            num = (position[0], position[1] - x)
-            if not num in locs:
-                locs[num] = 1
-            else:
-                first.append(num)
-        return (position[0], position[1] - delta)
-    elif direction == 3:
-        for x in range(delta):
-            num = (position[0] - x, position[1])
-            if not num in locs:
-                locs[num] = 1
-            else:
-                first.append(num)
-        return (position[0] - delta, position[1])
 
+def move(position, direction):
+    if direction == 0:
+        return (position[0], position[1] + 1)
+    elif direction == 1:
+        return (position[0] + 1, position[1])
+    elif direction == 2:
+        return (position[0], position[1] - 1)
+    elif direction == 3:
+        return (position[0] - 1, position[1])
 
 with open("input1.txt") as f:
     line = f.read().strip("\n").split(",")
     for x in line:
         nextDir(x[0])
-        pos = move(pos, int(x[1:]), direction)
+        for x in range(int(x[1:])):
+            pos = move(pos, direction)
+            if pos in locs:
+                first.append(pos)
+            else:
+                locs.add(pos)
 
 print "part 1:", abs(pos[0]) + abs(pos[1])
 print "part 2:", abs(first[0][0]) + abs(first[0][1])
