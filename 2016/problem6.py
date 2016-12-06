24ad7e14591606d2655904aa91753d17ab260472
Erik Godard <godarderik@gmail.com>
2016-12-06 00:17:21 -0500

    problem 6

---
+++ 2016/problem6.py
@@ -0,0 +1,8 @@
+from collections import *
+
+with open("input6.txt") as f:
+    lines = [line.strip("\n") for line in f]
+    out = [[line[k] for line in lines] for k in range(8)]
+    
+print "".join([Counter(x).most_common(1)[0][0] for x in out])
+print "".join([Counter(x).most_common()[::-1][0][0] for x in out])
