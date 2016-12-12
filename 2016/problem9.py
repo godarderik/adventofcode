1b08e656c01a2046836406db94b2427657cf9204
Erik Godard <godarderik@gmail.com>
2016-12-12 01:16:33 -0500

    finished problem 9

--- 2016/problem9.py
+++ 2016/problem9.py
@@ -1,5 +1,28 @@
+def get_length(inpt):
+    status = 0
+    ind = 0
+    count2 = 0
+    temp = ""
+    while ind < len(inpt):
+        char = inpt[ind]
+        if char == "(":
+            temp = ""
+            status = 1
+        elif status == 1 and char != ")":
+            temp += char
+        elif status == 1 and char == ")": 
+            temp = [int(x) for x in temp.split("x")]
+            count2 += get_length(inpt[ind+1: ind + temp[0]+1]) * temp[1]
+            ind += temp[0]
+            status = 0
+        else:
+            count2 += 1
+        ind += 1
+    return count2
+
+
 with open ("input9.txt") as f:
     for line in f:
         line = line.strip()
-        for char in line:
-            
+        print get_length(line)
+
