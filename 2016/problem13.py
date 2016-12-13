baf8aa8a4fe3dfe2f82098f330b3d2c9a75deb52
Erik Godard <godarderik@gmail.com>
2016-12-13 00:36:15 -0500

    clean up problem 13 code

--- 2016/problem13.py
+++ 2016/problem13.py
@@ -1,31 +1,17 @@
-from collections import *
-from itertools import *
-
 frontier = [(1,1,0)]
 explored = {}
-seen = {}
 
 def get_wall(tup):
-    if tup[0]< 0 or tup[1] < 0:
-        return False
-    num = tup[0] * tup[0] + 3 * tup[0] + 2 * tup[0] * tup[1] + tup[1] + tup[1] * tup[1]
-    num += 1364
-    return bin(num)[2:].count("1") % 2 == 0
+    num = tup[0] * tup[0] + 3 * tup[0] + 2 * tup[0] * tup[1] + tup[1] + tup[1] * tup[1] + 1364
+    return bin(num)[2:].count("1") % 2 == 0 and tup[0] >= 0 and tup[1] >= 0
 
-def get_n(tup, cost):
-    outArr = []
-    cand =  [(tup[0], tup[1] + 1, cost + 1), (tup[0] - 1, tup[1], cost + 1),(tup[0] + 1, tup[1], cost + 1),(tup[0], tup[1] - 1, cost + 1)]
-    for y in cand:
-        if get_wall(y):
-            outArr.append(y)
-    return outArr
+def get_next(tup):
+    cand = [(0,1), (0,-1), (1,0), (-1,0)]
+    return [(x[0] + tup[0], x[1] + tup[1], tup[2] + 1) for x in cand if get_wall((x[0] + tup[0], x[1] + tup[1]))]
 
 while len(frontier) > 0:
     new = frontier.pop()
     explored[(new[0], new[1])] = new[2]
-    if new[2] <= 50:
-        seen[(new[0], new[1])] = 1
-    for x in get_n((new[0], new[1]), new[2]):
-        if not (x[0], x[1]) in explored or explored[(x[0], x[1])] > x[2]:
-            frontier.append(x)
-print explored[(31,39)], len(seen) 
+    frontier += [x for x in get_next(new) if not (x[0], x[1]) in explored or explored[(x[0], x[1])] > x[2]]
+
+print explored[(31,39)], len([explored[x] for x in explored.keys() if explored[x] <= 50])
